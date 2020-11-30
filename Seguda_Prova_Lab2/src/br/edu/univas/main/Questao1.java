8d665f3030d76dce82c3e05f42071640578a3365
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2020-11-30 18:30:39 -0300

    Commit pré prova

---
+++ Seguda_Prova_Lab2/src/br/edu/univas/main/Questao1.java
@@ -0,0 +1,18 @@
+package br.edu.univas.main;
+
+import java.util.Scanner;
+
+public class Questao1 {
+	
+	public static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		scan.close();
+	}
+
+}
