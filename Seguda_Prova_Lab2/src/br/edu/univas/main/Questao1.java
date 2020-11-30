28677c34e7fed82fe8423555f01f198038a2bb8b
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2020-11-30 20:54:54 -0300

    imprime menu implementado

--- Seguda_Prova_Lab2/src/br/edu/univas/main/Questao1.java
+++ Seguda_Prova_Lab2/src/br/edu/univas/main/Questao1.java
@@ -5,14 +5,32 @@ import java.util.Scanner;
 public class Questao1 {
 	
 	public static Scanner scan = new Scanner(System.in);
-	
+	public static int rangeArray = 100;
 	public static void main(String[] args) {
 		
+		System.out.println("Bem vindo ao sistema de controle de Contas!!\n");
+		do {
+		imprimeMenu();
+		int option = scan.nextInt();
 		
 		
+		if (option == 3) {
+			
+			break;
+			
+		}
 		
-		
+		}while(true);
 		scan.close();
 	}
-
+	
+	public static void imprimeMenu () {
+		
+		
+		System.out.println("Digite uma das opções à seguir para continuar:\n\n");
+		System.out.println("1 - Cadastro de Conta: ");
+		System.out.println("2 - Ver Saldo: ");
+		System.out.println("3 - Sair:\n\n");
+		
+	}
 }
---
+++ Seguda_Prova_Lab2/src/br/edu/univas/vo/Conta.java
@@ -0,0 +1,10 @@
+package br.edu.univas.vo;
+
+public class Conta {
+
+	String descricao;
+	Float valor;
+	String dataVenc;
+	String tipo;
+	
+}
